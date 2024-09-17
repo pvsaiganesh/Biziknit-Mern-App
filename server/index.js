a2a8871cc33ddcc530733cba91968dc3621fc71f
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-17 14:21:00 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -15,6 +15,42 @@ app.listen(PORT, () => {
 });
 
 // All other GET requests not handled before will return our React app
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require("./model/Employee");
+const UserModel = require("./modal/registrants");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://49.37.151.17/32/employee");
+
+app.post("/login", (req, res) => {
+  const { phonenumber, password } = req.body;
+  UserModel.findOne({ phonenumber: phonenumber }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("The password is incorrect");
+      }
+    } else {
+      res.json("No record existed");
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employees) => res.json(employees))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("server is running");
+});
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
